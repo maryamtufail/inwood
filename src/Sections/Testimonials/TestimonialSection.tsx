48a915070e1900a10380157915e2bf96f72d6a4f
Maryam Tufail <maryamtufail78@gmail.com>
2024-09-15 22:39:08 +0500

    t-h-set

--- src/Sections/Testimonials/TestimonialSection.tsx
+++ src/Sections/Testimonials/TestimonialSection.tsx
@@ -2,9 +2,9 @@ import TestimonialSlider from '@/Components/Testimonials/TestimonialSlider';
 
 const TestimonialSection: React.FC = () => {
   return (
-    <section>
+    <section className="py-40">
       <div className="mx-auto text-center">
-        <h2 className="text-3xl lg:text-4xl font-bold  mb-4">Testimonials</h2>
+        <h2 className="text-3xl lg:text-4xl font-bold mb-4">Testimonials</h2>
         <p className="text-xl lg:text-2xl mb-8">Over 15,000 happy customers.</p>
         <TestimonialSlider />
       </div>
